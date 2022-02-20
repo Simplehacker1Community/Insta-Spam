04cbe61900b11112c639d32f2eea0a4ab3d2b4e3
Simplehacker1Community <93964893+Simplehacker1Community@users.noreply.github.com>
2022-02-20 10:17:04 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 apt update 
 apt upgrade -y
 apt install python -y
+pip install --upgrade pip
 pip install instabot==0.84.0
 pip install requests
 pip install python-dotenv
+pip install instaspam2
+clear
+apt install wget curl nano fish -y
